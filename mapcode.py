a13bcd198a7275a1614bd35f48478722800f92cb
Chris99-tech <68871477+Chris99-tech@users.noreply.github.com>
2020-07-28 12:48:06 -0400

    code

---
+++ mapcode.py
@@ -0,0 +1,8 @@
+  #import packages
+
+import pandas as pd
+import numpy as np
+schoollocation_sl=pd.read_csv("https://coronavirus-resources.esri.com/datasets/0e8df2dcbbc54e13833344e2ca8c0fa4_0")
+Countycovid=pd.read_csv("https://covidtracking.com/api/v1/states/info.csv")
+print(Countycovid)
+
