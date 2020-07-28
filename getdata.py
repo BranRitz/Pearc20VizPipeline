819cc73b8604f3f7ae5c6296ce13d747e2916317
gtoutin <57967331+gtoutin@users.noreply.github.com>
2020-07-28 11:49:20 -0500

    Update getdata.py

--- getdata.py
+++ getdata.py
@@ -17,15 +17,16 @@ def pullcovid():
 
 def pullpublicschool():
     '''
-    Function to pull school location data. Only needs to run once (how often do new schools get added?)
+    Function to pull school location data. Only needs to run once
     :return:
     '''
+
     # Need: county FIPS, latitude, longitude, school name, school id
     schooldata = requests.get("https://services1.arcgis.com/Ua5sjt3LWTPigjyD/arcgis/rest/services/Public_School_Location_201819/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json")
     schooldata.json()     # school data is now a dictionary
-    schoollist = schooldata["fields"]   # dictionary of all the schools
+    schoollist = schooldata["features"]   # dictionary of all the schools
 
-    print(schoollist)
+    schoolarr = np.array(schoollist[1].keys())  # Header of array is names of the attributes
 
     for school in schoollist:
-        
+        np.concatenate(schoolarr, school.values())  # adding the values of the school to the array
