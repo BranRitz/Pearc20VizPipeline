457dba7d684a9c922ca3d0e7db39108827d2ba23
gtoutin <57967331+gtoutin@users.noreply.github.com>
2020-07-29 09:55:30 -0500

    Update getdata.py

--- getdata.py
+++ getdata.py
@@ -62,18 +62,21 @@ def pullpublicschool():
     lon_arr = []
     zip_arr = []
     fips_arr = []
-    states = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME',  'MD', 'MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT',  'VT', 'VA',  'WA','WV', 'WI','WY' ]
+    states = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS',
+              'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC',
+              'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY']
     # Need: county FIPS, latitude, longitude, school name, school id
-    #schooldata = requests.get("https://services1.arcgis.com/Ua5sjt3LWTPigjyD/arcgis/rest/services/Public_School_Location_201819/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json")
+    # schooldata = requests.get("https://services1.arcgis.com/Ua5sjt3LWTPigjyD/arcgis/rest/services/Public_School_Location_201819/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json")
     for state in states:
-    
-        schooldata = requests.get("https://services1.arcgis.com/Ua5sjt3LWTPigjyD/arcgis/rest/services/Public_School_Location_201819/FeatureServer/0/query?where=STATE%20%3D%20'"+state+"'&outFields=NAME,STREET,CITY,STATE,ZIP,CNTY,NMCNTY,LAT,LON&outSR=4326&f=json")
-        schools = schooldata.json()     # school data is now a dictionary
 
-        schoollist = schools["features"]   # dictionary of all the schools
-   
+        schooldata = requests.get(
+            "https://services1.arcgis.com/Ua5sjt3LWTPigjyD/arcgis/rest/services/Public_School_Location_201819/FeatureServer/0/query?where=STATE%20%3D%20'" + state + "'&outFields=NAME,STREET,CITY,STATE,ZIP,CNTY,NMCNTY,LAT,LON&outSR=4326&f=json")
+        schools = schooldata.json()  # school data is now a dictionary
+
+        schoollist = schools["features"]  # dictionary of all the schools
+
         # schoolarr = np.array(schoollist[0].keys())  # Header of array is names of the attributes
-        
+
         for school in schoollist:
             attr = school["attributes"]
             school_name_arr.append(attr["NAME"])
@@ -85,36 +88,39 @@ def pullpublicschool():
             zip_arr.append(attr["ZIP"])
             lat_arr.append(attr["LAT"])
             lon_arr.append(attr["LON"])
-        
-        #np.concatenate(schoolarr, school.values())  # adding the values of the school to the array
 
-    diction = {"name": school_name_arr, "street": street_addr_arr, "city": city_arr, 
-        "county_name": county_arr, "fips": fips_arr, "state":state_arr, "zip": zip_arr, "lat": lat_arr, "lon": lon_arr}
+        # np.concatenate(schoolarr, school.values())  # adding the values of the school to the array
+
+    diction = {"name": school_name_arr, "street": street_addr_arr, "city": city_arr,
+               "county_name": county_arr, "fips": fips_arr, "state": state_arr, "zip": zip_arr, "lat": lat_arr,
+               "lon": lon_arr}
     return json.dumps(diction)
+
+
 def generateSchoolMap():
-    #load data first time only
-    if(not path.exists("schools.json")):
+    # load data first time only
+    if (not path.exists("schools.json")):
         schoollist = pullpublicschool()
         myfile = open("schools.json", "w")
         myfile.write(schoollist)
 
+    df = pd.read_json(open("schools.json", "r"), lines=True)
 
-    df = pd.read_json(open("schools.json", "r"),lines=True)
-
-    #make the map
+    # make the map
     fig = go.Figure(data=go.Scattergeo(
-        lon = df['lon'][0],
-        lat = df['lat'][0],
-        text = df['name'][0],
-        mode = 'markers',
-        ))
+        lon=df['lon'][0],
+        lat=df['lat'][0],
+        text=df['name'][0],
+        mode='markers',
+    ))
 
     fig.update_layout(
-        title = 'Public schools across America',
+        title='Public schools across America',
         geo_scope='usa',
     )
 
     fig.show()
+
 
 generateSchoolMap()
 pullcovid()
